e06de0fb77f76b39603fc52ef5d6bfc7f8bfdcfd
sssun <98_0509@naver.com>
2024-04-27 21:48:15 +0900

    first_commit

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+      home: Scaffold(
+    body: TestWidget(),
+  ),),); //flutter 로 앱을 만들 때 가장 기본이 되는 앱 단위
+}
+
+class TestWidget extends StatelessWidget {
+  const TestWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Center(
+        child: Text(
+          'Hello, Flutter',
+          style: TextStyle(
+            fontSize: 40,
+            color: Colors.black,
+          ),
+        ),
+      ),
+    );
+  }
+}
