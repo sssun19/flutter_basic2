111190fcdcff77dc5f60a9a20b47b063cc18e495
sssun <98_0509@naver.com>
2024-04-30 04:09:12 +0900

    Column 상하배치

---
+++ lib/main2.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text('Widget 을 상하로 배치하기'),
+      ),
+      body: Body(),
+    ),
+  ));
+}
+
+class Body extends StatelessWidget {
+  const Body({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      child: Column(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.center, //column은 center 말고 mainAxisAlignment 로 가운데 정렬 (상하정렬)
+        crossAxisAlignment: CrossAxisAlignment.center, //좌우정렬
+        children: [
+          Container(
+            width: 100,
+            height: 80,
+            color: Colors.red,
+            child: Text('Container 1'),
+          ),
+          Container(
+            width: 100,
+            height: 80,
+            color: Colors.green,
+            child: Text('Container 2'),
+          ),
+          Container(
+            width: 100,
+            height: 80,
+            color: Colors.blue,
+            child: Text('Container 3'),
+          ),
+        ],
+      ),
+    );
+  }
+}
