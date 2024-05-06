85e2dda4cc44a1658f46343cfc109872441da8bb
sssun <98_0509@naver.com>
2024-05-07 00:51:06 +0900

    Slider

--- lib/main7.dart
+++ lib/main7.dart
@@ -28,6 +28,7 @@ class Body extends StatelessWidget {
       children: [
         TestCheckBox(),
         TestRadioButton(),
+        TestSlider(),
       ],
     );
   }
@@ -123,7 +124,7 @@ class _TestRadioButtonState extends State<TestRadioButton> {
             }
           }),
         ),
-        ListTile(
+        ListTile( // 터치 범위 확장 가능
           leading: Radio<TestRadioValue>(
             value: TestRadioValue.test3,
             groupValue: selectValue,
@@ -135,6 +136,35 @@ class _TestRadioButtonState extends State<TestRadioButton> {
               selectValue = TestRadioValue.test3;
             }
           }),
+        ),
+      ],
+    );
+  }
+}
+
+class TestSlider extends StatefulWidget {
+  const TestSlider({super.key});
+
+  @override
+  State<TestSlider> createState() => _TestSliderState();
+}
+
+class _TestSliderState extends State<TestSlider> {
+  double value = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text('${value.round()}'),
+        Slider(
+          value: value,
+          onChanged: (newValue) => setState(() => value = newValue),
+          divisions: 100,
+          max: 100,
+          min: 0,
+          label: value.round().toString(),
+          activeColor: Colors.red,
         ),
       ],
     );
