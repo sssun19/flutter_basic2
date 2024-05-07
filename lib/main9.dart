17d1e9d31c67a2118d7d5a9b8537135061856f62
sssun <98_0509@naver.com>
2024-05-07 20:07:26 +0900

    오픈소스 외부 라이브러리

---
+++ lib/main9.dart
@@ -0,0 +1,48 @@
+import 'package:english_words/english_words.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            'Flutter에서 외부 라이브러리 사용하기',
+            style: TextStyle(color: Colors.white),
+          ),
+          centerTitle: true,
+        ),
+        body: const Body(),
+      ),
+    ),
+  );
+}
+
+class Body extends StatelessWidget {
+  const Body({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const RandomWords();
+  }
+}
+
+class RandomWords extends StatelessWidget {
+  const RandomWords({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final wordList = generateWordPairs().take(5).toList();
+    final widgets = wordList.map(
+      (word) => Text(
+        word.asCamelCase,
+        style: const TextStyle(fontSize: 32),
+      ),
+    ).toList();
+
+    return Column(
+      children: widgets,
+    );
+  }
+}
