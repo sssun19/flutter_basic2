915b4fa595d034d9ce84717dee42f0598e5fa70d
sssun <98_0509@naver.com>
2024-05-07 18:58:35 +0900

    callback 함수

---
+++ lib/main8.dart
@@ -0,0 +1,92 @@
+import 'dart:developer';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            'Flutter의 Callback',
+            style: TextStyle(color: Colors.white),
+          ),
+          centerTitle: true,
+          backgroundColor: Colors.blue,
+        ),
+        body: const Body(),
+      ),
+    ),
+  );
+}
+
+class Body extends StatelessWidget {
+  const Body({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const TestWidget();
+  }
+}
+
+class TestWidget extends StatefulWidget {
+  const TestWidget({super.key});
+
+  @override
+  State<TestWidget> createState() => _TestWidgetState();
+}
+
+class _TestWidgetState extends State<TestWidget> {
+  int value = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(
+          'Count: $value',
+          style: const TextStyle(fontSize: 30),
+        ),
+        TestButton(addCounter),
+      ],
+    );
+  }
+
+  // void addCounter() {
+  //   setState(() => ++value);
+  // }
+
+  void addCounter(int addValue) => setState(() => value = addValue + value);
+}
+
+class TestButton extends StatelessWidget {
+  const TestButton(this.callback, {super.key});
+
+  // final VoidCallback callback;
+  final Function(int) callback;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 8),
+      width: double.infinity,
+      child: InkWell(
+        onTap: () => {log('Tab!'), callback.call(1)},
+        onDoubleTap: () => callback.call(5),
+        onLongPress: () => callback.call(10),
+        child: Center(
+          child: Container(
+            padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 8),
+            decoration: BoxDecoration(border: Border.all()),
+            child: const Text(
+              'Up Counter',
+              style: TextStyle(fontSize: 24),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
