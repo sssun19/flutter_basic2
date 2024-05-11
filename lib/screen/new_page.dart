d2a83ff87fef6a649dfc2677f7db1c17ae639716
sssun <98_0509@naver.com>
2024-05-12 00:00:00 +0900

    page2 이동

--- lib/screen/new_page.dart
+++ lib/screen/new_page.dart
@@ -1,3 +1,5 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 
 class NewPage extends StatelessWidget {
@@ -10,11 +12,58 @@ class NewPage extends StatelessWidget {
         title: const Text('Welcome new Page'),
       ),
       body: Center(
-        child: TextButton(
-          child: const Text('Go to Back'),
-          onPressed: () {
-            Navigator.pop(context);
-          },
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextButton(
+              child: const Text('Go to Back'),
+              onPressed: () {
+                Navigator.pop(context);
+              },
+            ),
+            TextButton(
+              child: const Text('Go to New Page2'),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const NewPage2()),
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class NewPage2 extends StatelessWidget {
+  const NewPage2({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Welcome to New Page2'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            TextButton(
+              child: const Text('Go to Back'),
+              onPressed: () {
+                Navigator.pop(context);
+              },
+            ),
+            TextButton(
+              child: const Text('Go to Home'),
+              onPressed: () {
+                Navigator.pop(context);
+              },
+            ),
+          ],
         ),
       ),
     );
